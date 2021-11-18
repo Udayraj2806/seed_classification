19938f669158970fe9a39bf6522e6ce4afe75e42
Udaykumar Sanjay Gadikar <74957465+Udayraj2806@users.noreply.github.com>
2021-11-18 22:13:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,request,jsonify
 import numpy as np
 
 import main
-from main import Decision_Node,classify,Leaf,Question
+from main import Decision_Node,Leaf,Question
 import pickle
 
 model = pickle.load(open('model.pkl','rb'))
